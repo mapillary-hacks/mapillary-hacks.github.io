c3ca65d1cd0a797e563b144d5ea3882f505a8619
mapillary-hacks <chris@mapillary.com>
2017-03-24 17:25:28 +0000

    Add files via upload

---
+++ hackathon/malaysia/here/toggle.js
@@ -0,0 +1,29 @@
+var toggleableLayerIds = [ 'temp-layer', 'trees-layer', 'ndvi-layer', 'mapillary' ];
+
+for (var i = 0; i < toggleableLayerIds.length; i++) {
+    var id = toggleableLayerIds[i];
+
+    var link = document.createElement('a');
+    link.href = '#';
+    link.className = '';
+    link.textContent = id;
+
+    link.onclick = function (e) {
+        var clickedLayer = this.textContent;
+        e.preventDefault();
+        e.stopPropagation();
+
+        var visibility = map.getLayoutProperty(clickedLayer, 'visibility');
+
+        if (visibility === 'visible') {
+            map.setLayoutProperty(clickedLayer, 'visibility', 'none');
+            this.className = '';
+        } else {
+            this.className = 'active';
+            map.setLayoutProperty(clickedLayer, 'visibility', 'visible');
+        }
+    };
+
+    var layers = document.getElementById('menu');
+    layers.appendChild(link);
+}
