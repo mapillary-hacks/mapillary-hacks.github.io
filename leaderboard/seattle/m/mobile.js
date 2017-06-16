1f5239b654a081a291b6b5f71f98bd0a892a0bd4
Christopher Beddow <christopher@mapillary.com>
2017-06-16 01:23:03 -0600

    Add files via upload

--- leaderboard/seattle/m/mobile.js
+++ leaderboard/seattle/m/mobile.js
@@ -1,62 +1,98 @@
 var count = 0;
+var count3 = 0;
+var maxdate = Date.now();
+var mindate = (1496966400000); //set to show epoch milliseconds of the start date
+
+function mlyInit() {
+  console.log("adding Mapillary source");
+  var mapillarySource = {
+      type: 'vector',
+      tiles: ['https://d25uarhxywzl1j.cloudfront.net/v0.1/{z}/{x}/{y}.mvt'],
+      minzoom: 0,
+      maxzoom: 14,
+  };
+  map.addSource('mapillary', mapillarySource);
+}
 
 function mlyAdd() {
     console.log(count);
     switch (count) {
       case 0:
         mapillaryBtn.style.backgroundColor = "#00bcff";
-        var mapillarySource = {
-            type: 'vector',
-            tiles: ['https://d25uarhxywzl1j.cloudfront.net/v0.1/{z}/{x}/{y}.mvt'],
-            minzoom: 0,
-            maxzoom: 14,
-        };
-        map.addSource('mapillary', mapillarySource);
-        map.addLayer({
-            'id': 'mapillary-all',
-            'type': 'circle',
-            'source': 'mapillary',
-            'source-layer': 'mapillary-sequence-overview',
-            'paint': {
-                'circle-opacity': 0.75,
-                'circle-color':   'rgb(53, 175, 109)',
-                'circle-radius':   2
-            }
-        }, 'mapillary-o');
-        map.addLayer({
-           'id': 'mapillary-all2',
-           'type': 'line',
-           'source': 'mapillary',
-           'source-layer': 'mapillary-sequences',
-           'layout': {
-               'line-cap': 'round',
-               'line-join': 'round'
-           },
-           'paint': {
-               'line-opacity': 0.75,
-               'line-color':   'rgb(53, 175, 109)',
-               'line-width':   2
-           }
-        }, 'mapillary');
-        count = 1;
-        break;
+        try {
+          var mapillarySource = {
+              type: 'vector',
+              tiles: ['https://d25uarhxywzl1j.cloudfront.net/v0.1/{z}/{x}/{y}.mvt'],
+              minzoom: 0,
+              maxzoom: 14,
+          };
+          map.addSource('mapillarySource', mapillarySource);
+        } catch (err) {
+          // Do nothing the layer already exists
+        }
+        try {
+          map.addLayer({
+              'id': 'mapillary-all',
+              'type': 'circle',
+              'source': 'mapillarySource',
+              'source-layer': 'mapillary-sequence-overview',
+              'paint': {
+                  'circle-opacity': 0.75,
+                  'circle-color':   'rgb(53, 175, 109)',
+                  'circle-radius':   2
+              }
+          }, 'mapillary-o');
+          map.addLayer({
+             'id': 'mapillary-all2',
+             'type': 'line',
+             'source': 'mapillarySource',
+             'source-layer': 'mapillary-sequences',
+             'layout': {
+                 'line-cap': 'round',
+                 'line-join': 'round'
+             },
+             'paint': {
+                 'line-opacity': 0.75,
+                 'line-color':   'rgb(53, 175, 109)',
+                 'line-width':   2
+             }
+          }, 'mapillary');
+          count = 1;
+          break;
+        } catch (err) {
+          //Do nothing
+        }
       case 1:
         mapillaryBtn.style.background = "#5D6671";
-        map.removeSource('mapillary');
-        map.removeLayer('mapillary-all');
-        map.removeLayer('mapillary-all2');
+        try {
+          map.removeLayer('mapillary','mapillary-o');
+          map.removeSource('mapillarySource');
+          mlyInit();
+        } catch (err) {
+          // Do nothing the layers just does not exists
+        }
+        try {
+          map.removeLayer('mapillary-all');
+          map.removeLayer('mapillary-all2');
+        } catch (err) {
+          // Do nothing
+        }
         count = 0;
         break;
-    };
-};
+    }
+}
 
 function statsDisplay(){
   $("#map").empty;
   $("#statsdisplay").css('background', "#00bcff");
   $("#mapdisplay").css('background', "#5D6671");
   $("#map").load("stats.html");
-  $("#slider").disable;
-
+  $("#slider").disable
+  if (count == 1) {
+    mlyAdd();
+    count3 = 3;
+    console.log('count3 equals ' + count3);
+  }
 }
 function mapDisplay(){
   $("#map").empty;
@@ -64,39 +100,12 @@ function mapDisplay(){
   $("#statsdisplay").css('background', "#5D6671");
   $("#map").load("map.html");
   $("#slider").enable;
-  if (count == 1) {
-    var mapillarySource = {
-        type: 'vector',
-        tiles: ['https://d25uarhxywzl1j.cloudfront.net/v0.1/{z}/{x}/{y}.mvt'],
-        minzoom: 0,
-        maxzoom: 14,
-    };
-    map.addSource('mapillary', mapillarySource);
-    map.addLayer({
-        'id': 'mapillary-all',
-        'type': 'circle',
-        'source': 'mapillary',
-        'source-layer': 'mapillary-sequence-overview',
-        'paint': {
-            'circle-opacity': 0.75,
-            'circle-color':   'rgb(53, 175, 109)',
-            'circle-radius':   2
-        }
-    }, 'mapillary-o');
-    map.addLayer({
-       'id': 'mapillary-all2',
-       'type': 'line',
-       'source': 'mapillary',
-       'source-layer': 'mapillary-sequences',
-       'layout': {
-           'line-cap': 'round',
-           'line-join': 'round'
-       },
-       'paint': {
-           'line-opacity': 0.75,
-           'line-color':   'rgb(53, 175, 109)',
-           'line-width':   2
-       }
-    }, 'mapillary');
+  if (count3 == 3 ) {
+    setTimeout(function() {
+      mlyAdd();
+      count3 = 0;
+      console.log('count3 equals ' + count3);
+    }, 1000);
   }
+
 }
