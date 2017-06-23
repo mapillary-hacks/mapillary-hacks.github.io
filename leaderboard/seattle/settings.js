e4e5a88e58b0e8eb602692819467b944ade05bfa
Edoardo Neerhut <ed@mapillary.com>
2017-06-23 15:01:23 +1000

    Update dates

--- leaderboard/seattle/settings.js
+++ leaderboard/seattle/settings.js
@@ -3,13 +3,13 @@ var med = [];
 var low = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25];
 
 var maxdate = Date.now(); //don't change this
-var mindate = (1496966400000); //set to show epoch milliseconds of the start date
+var mindate = (1498204800000); //set to show epoch milliseconds of the start date
 
 var partnerLogo = 'https://secure.meetupstatic.com/photos/event/f/7/a/highres_438483962.jpeg';
 var partnerLink = 'https://www.meetup.com/MaptimeSEA/'
 
 var challengeTitle = 'Seattle Challenge'; //text in the slider
-var challengeDate = 'June 9 - June 30, 2017'
+var challengeDate = 'June 23rd - July 31st, 2017'
 var docTitle = 'Complete the Map: Seattle - Mapillary'; //text next to favicon on browser window
 
 var challengeLng = -122.3111343383789; //center of the map
