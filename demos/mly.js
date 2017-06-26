4f252db677b450ccd72c6a639194b78b7505132b
Christopher Beddow <christopher@mapillary.com>
2017-06-26 15:43:29 -0600

    Add files via upload

---
+++ demos/mly.js
@@ -0,0 +1,14 @@
+var imgkey = 0;
+var mly = new Mapillary.Viewer(
+      'mly',
+      // Replace this with your own client ID from mapillary.com
+      'QjI1NnU0aG5FZFZISE56U3R5aWN4ZzowODkzY2RjNjM1ZmEwYTVi',
+      null,
+    {
+      component: {
+                    cache: false,
+                    cover: false,
+                    direction: false,
+                    sequence: false
+                },
+    });
