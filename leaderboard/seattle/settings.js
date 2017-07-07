3d3b4eb467b4df7ebca6d32ba5c61545d2ac01f5
Christopher Beddow <christopher@mapillary.com>
2017-07-06 22:48:24 -0600

    Update settings.js

--- leaderboard/seattle/settings.js
+++ leaderboard/seattle/settings.js
@@ -4,7 +4,7 @@ var low = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25];
 
 var maxdate = Date.now(); //don't change this
 var mindate = (1498204800000); //set to show epoch milliseconds of the start date
-var mindate2 = new Date(mindate).toISOString()
+var mindate2 = new Date(mindate).toISOString();
 
 var partnerLogo = 'https://secure.meetupstatic.com/photos/event/f/7/a/highres_438483962.jpeg';
 var partnerLink = 'https://www.meetup.com/MaptimeSEA/'
