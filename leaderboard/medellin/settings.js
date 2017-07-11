82b7aec7e1e04aad57ffbdb667a50702816f4236
Edoardo Neerhut <Edoardo Neerhut>
2017-07-11 18:34:17 +1000

    Add information for Colombia

--- leaderboard/medellin/settings.js
+++ leaderboard/medellin/settings.js
@@ -3,18 +3,18 @@ var med = [];
 var low = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25];
 
 var maxdate = Date.now(); //don't change this
-var mindate = (1498204800000); //set to show epoch milliseconds of the start date
+var mindate = (1500008400000); //set to show epoch milliseconds of the start date
 var mindate2 = new Date(mindate).toISOString();
 
-var partnerLogo = 'https://secure.meetupstatic.com/photos/event/f/7/a/highres_438483962.jpeg';
-var partnerLink = 'https://www.meetup.com/MaptimeSEA/'
+var partnerLogo = 'http://blog.openstreetmap.co/img/osm-co.png';
+var partnerLink = 'http://blog.openstreetmap.co/'
 
-var challengeTitle = 'Seattle Challenge'; //text in the slider
-var challengeDate = 'June 23rd - July 31st, 2017'
-var docTitle = 'Complete the Map: Seattle - Mapillary'; //text next to favicon on browser window
+var challengeTitle = 'Medellín Challenge'; //text in the slider
+var challengeDate = 'July 14th - July 31st, 2017'
+var docTitle = 'Complete the Map: Medellín - Mapillary'; //text next to favicon on browser window
 
-var challengeLng = -122.3111343383789; //center of the map
-var challengeLat = 47.61761985489538; //center of the map
+var challengeLng = -75.588014602661133; //center of the map
+var challengeLat = 6.259724583206608; //center of the map
 var challengeZoom = 14; //zoom for desktop browser
 var mobileZoom = 12; //should be challengeZoom minus 2 usually
 
