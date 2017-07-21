4d4972fc2b0bf573e07fff5b58fd584526b9a457
Edoardo Neerhut <ed@mapillary.com>
2017-07-21 14:45:15 +1000

    Update start date

--- leaderboard/medellin/settings.js
+++ leaderboard/medellin/settings.js
@@ -3,7 +3,7 @@ var med = [];
 var low = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25];
 
 var maxdate = Date.now(); //don't change this
-var mindate = (1500008400000); //set to show epoch milliseconds of the start date
+var mindate = (1500613200000); //set to show epoch milliseconds of the start date
 var mindate2 = new Date(mindate).toISOString();
 
 var partnerLogo = 'http://blog.openstreetmap.co/img/osm-co.png';
