5d52433670444d7a6888c5c580d4c8c98e6381c5
Edoardo Neerhut <ed@mapillary.com>
2017-08-11 17:28:40 +1000

    Test zoom levels

--- leaderboard/kyiv/settings.js
+++ leaderboard/kyiv/settings.js
@@ -3,18 +3,18 @@ var med = [];
 var low = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25];
 
 var maxdate = Date.now(); //don't change this
-var mindate = (1498204800000); //set to show epoch milliseconds of the start date
+var mindate = (1502769600000); //set to show epoch milliseconds of the start date
 var mindate2 = new Date(mindate).toISOString();
 
 var partnerLogo = 'https://secure.meetupstatic.com/photos/event/f/7/a/highres_438483962.jpeg';
 var partnerLink = 'https://www.meetup.com/MaptimeSEA/'
 
-var challengeTitle = 'Seattle Challenge'; //text in the slider
-var challengeDate = 'June 23rd - July 31st, 2017'
-var docTitle = 'Complete the Map: Seattle - Mapillary'; //text next to favicon on browser window
+var challengeTitle = 'Kyiv Challenge'; //text in the slider
+var challengeDate = 'August 15th - September 15th, 2017'
+var docTitle = 'Complete the Map: Kyiv - Mapillary'; //text next to favicon on browser window
 
-var challengeLng = -122.3111343383789; //center of the map
-var challengeLat = 47.61761985489538; //center of the map
+var challengeLng = 30.449467; //center of the map
+var challengeLat = 50.417426; //center of the map
 var challengeZoom = 14; //zoom for desktop browser
 var mobileZoom = 12; //should be challengeZoom minus 2 usually
 
