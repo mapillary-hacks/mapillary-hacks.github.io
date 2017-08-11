0a3978052194f5ab6e7ecb57e8aac65fe72ee56d
Edoardo Neerhut <ed@mapillary.com>
2017-08-11 14:36:58 +1000

    Add basic settings for Ottawa

---
+++ leaderboard/ottawa/settings.js
@@ -0,0 +1,23 @@
+var high = [];
+var med = [];
+var low = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25];
+
+var maxdate = Date.now(); //don't change this
+var mindate = (1502769600000); //set to show epoch milliseconds of the start date
+var mindate2 = new Date(mindate).toISOString();
+
+var partnerLogo = 'https://secure.meetupstatic.com/photos/event/f/7/a/highres_438483962.jpeg';
+var partnerLink = 'https://www.meetup.com/MaptimeSEA/'
+
+var challengeTitle = 'Ottawa Challenge'; //text in the slider
+var challengeDate = 'August 15 - September 15, 2017'
+var docTitle = 'Complete the Map: Ottawa - Mapillary'; //text next to favicon on browser window
+
+var challengeLng = -75.6972; //center of the map
+var challengeLat = 45.4215; //center of the map
+var challengeZoom = 14; //zoom for desktop browser
+var mobileZoom = 12; //should be challengeZoom minus 2 usually
+
+var facebookShare = 'url.com'; //link for facebook sharing
+var twitterShare = 'url.com'; //link for twitter sharing
+var emailShare = 'mail.com'; //link for email sharing
