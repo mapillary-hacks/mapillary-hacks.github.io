7bcb538890ce3d17fadcfed0a9108e3b4d3a034b
Edoardo Neerhut <Edoardo Neerhut>
2017-08-16 15:07:31 +1000

    Change start date for Kyiv

--- leaderboard/kyiv/settings.js
+++ leaderboard/kyiv/settings.js
@@ -1,16 +1,16 @@
 var high = [];
 var med = [];
-var low = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25];
+var low = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
 
 var maxdate = Date.now(); //don't change this
-var mindate = (1502769600000); //set to show epoch milliseconds of the start date
+var mindate = (1503025200000); //set to show epoch milliseconds of the start date
 var mindate2 = new Date(mindate).toISOString();
 
 var partnerLogo = 'https://secure.meetupstatic.com/photos/event/f/7/a/highres_438483962.jpeg';
 var partnerLink = 'https://www.meetup.com/MaptimeSEA/'
 
 var challengeTitle = 'Kyiv Challenge'; //text in the slider
-var challengeDate = 'August 15th - September 15th, 2017'
+var challengeDate = 'August 18th - September 15th, 2017'
 var docTitle = 'Complete the Map: Kyiv - Mapillary'; //text next to favicon on browser window
 
 var challengeLng = 30.449467; //center of the map
