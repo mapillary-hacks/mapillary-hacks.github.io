c398fd8c61feddcade664b001bf8bc45970f9738
Edoardo Neerhut <Edoardo Neerhut>
2017-08-16 16:12:55 +1000

    Add challenge for Moscow

---
+++ leaderboard/moscow/settings.js
@@ -0,0 +1,23 @@
+var high = [];
+var med = [];
+var low = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16];
+
+var maxdate = Date.now(); //don't change this
+var mindate = (1503630000000); //set to show epoch milliseconds of the start date
+var mindate2 = new Date(mindate).toISOString();
+
+var partnerLogo = 'https://secure.meetupstatic.com/photos/event/f/7/a/highres_438483962.jpeg';
+var partnerLink = 'https://www.meetup.com/MaptimeSEA/'
+
+var challengeTitle = 'Moscow Challenge'; //text in the slider
+var challengeDate = 'August 25th - September 22nd, 2017'
+var docTitle = 'Complete the Map: Moscow - Mapillary'; //text next to favicon on browser window
+
+var challengeLng = 37.623281; //center of the map
+var challengeLat = 55.756486; //center of the map
+var challengeZoom = 12; //zoom for desktop browser
+var mobileZoom = 10; //should be challengeZoom minus 2 usually
+
+var facebookShare = 'url.com'; //link for facebook sharing
+var twitterShare = 'url.com'; //link for twitter sharing
+var emailShare = 'mail.com'; //link for email sharing
