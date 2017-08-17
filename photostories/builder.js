4434b3ed51e87bb115a01b9fbe30be17a6078690
cbeddow <christopher.beddow@gmail.com>
2017-08-17 14:19:45 +0100

    allow for xy coords set in images

--- photostories/builder.js
+++ photostories/builder.js
@@ -10,7 +10,7 @@ function loadSlides () {
   if (changeValue > 0) {
     for (i = 0; i < changeValue; i++) {
       var x = (currentSlideCount++)+1;
-      $("#slideGallery").append("<div class='square' style='color:#5D6671;background-color:white' id='slide" + (x) + "'><b>Slide " + (x) + "</b><br><form>Image Key:<br><textarea name='content' id='key" + x + "' class='smallTextArea' width=100% height=25 style='color:#5D6671;background-color:#D7DADD''></textarea><br>Slide Title:<br><textarea name='content' id='title" + x + "' class='smallTextArea' width=100% height=25  style='color:#5D6671;background-color:#D7DADD''></textarea><br>Content:<br><textarea name='content' id='content" + x + "' class='contentTextArea' width=100% height=200  style='color:#5D6671;background-color:#D7DADD''></textarea></form></div>")
+      $("#slideGallery").append("<div class='square' style='color:#5D6671;background-color:white' id='slide" + (x) + "'><b>Slide " + (x) + "</b><br><form>Image Key:<br><textarea name='content' id='key" + x + "' class='smallTextArea' width=100% height=25 style='color:#5D6671;background-color:#D7DADD''></textarea><br><form>x (optional):<br><textarea name='content' id='xcoord" + x + "' class='smallTextArea' width=100% height=25 style='color:#5D6671;background-color:#D7DADD''></textarea><br><form>y (optional):<br><textarea name='content' id='ycoord" + x + "' class='smallTextArea' width=100% height=25 style='color:#5D6671;background-color:#D7DADD''></textarea><br>Slide Title:<br><textarea name='content' id='title" + x + "' class='smallTextArea' width=100% height=25  style='color:#5D6671;background-color:#D7DADD''></textarea><br>Content:<br><textarea name='content' id='content" + x + "' class='contentTextArea' width=100% height=200 style='color:#5D6671;background-color:#D7DADD''></textarea></form></div>")
     };
   } else if (changeValue < 0) {
     for ( i = 0; i < changeValue * -1; i++) {
@@ -27,18 +27,24 @@ function loadOutput() {
   var images = [];
   var titles = [];
   var content = [];
+  var xcoords = [];
+  var ycoords = [];
   var project_title = "'" + document.getElementById("bigtitle").value + "'";
   var project_subtitle = "'" + document.getElementById("subtitle").value + "'";
   for (i=1; i <= currentSlideCount; i++) {
     images.push("'" + document.getElementById("key"+i).value + "'");
     titles.push("'" + document.getElementById("title"+i).value + "'");
     content.push("'" + document.getElementById("content"+i).value + "'");
+    xcoords.push("'" + document.getElementById("xcoord"+i).value + "'");
+    ycoords.push("'" + document.getElementById("ycoord"+i).value + "'");
   };
   $("#outputForm").append("var project_title = " + project_title + ";\n");
   $("#outputForm").append("var project_subtitle = " + project_subtitle + ";\n");
   $("#outputForm").append("var images = [" + images + "];\n");
   $("#outputForm").append("var titles = [" + titles + "];\n");
   $("#outputForm").append("var content = [" + content + "];\n");
+  $("#outputForm").append("var xcoords = [" + xcoords + "];\n");
+  $("#outputForm").append("var ycoords = [" + ycoords + "];\n");
 };
 
 function saveTextAsFile() {
