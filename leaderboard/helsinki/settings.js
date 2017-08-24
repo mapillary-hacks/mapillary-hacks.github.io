f026c37022836b5db65dd1e7a64f8e457779b37a
Edoardo Neerhut <ed@mapillary.com>
2017-08-24 16:02:25 +1000

    Change date

--- leaderboard/helsinki/settings.js
+++ leaderboard/helsinki/settings.js
@@ -3,10 +3,10 @@ var med = [];
 var low = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25];
 
 var maxdate = Date.now(); //don't change this
-var mindate = (1502769600000); //set to show epoch milliseconds of the start date
+var mindate = (1503608400000); //set to show epoch milliseconds of the start date
 var mindate2 = new Date(mindate).toISOString();
 
-var partnerLogo = 'https://secure.meetupstatic.com/photos/event/f/7/a/highres_438483962.jpeg';
+var partnerLogo = 'https://d3rqg5ndc8pbid.cloudfront.net/original/1X/695bb1b126407a23bf745acfa61fcb3b9682debd.png';
 var partnerLink = 'https://www.meetup.com/MaptimeSEA/'
 
 var challengeTitle = 'Helsinki Challenge'; //text in the slider
@@ -15,8 +15,8 @@ var docTitle = 'Complete the Map: Helsinki - Mapillary'; //text next to favicon 
 
 var challengeLng = 24.9384; //center of the map
 var challengeLat = 60.1699; //center of the map
-var challengeZoom = 13; //zoom for desktop browser
-var mobileZoom = 11; //should be challengeZoom minus 2 usually
+var challengeZoom = 14; //zoom for desktop browser
+var mobileZoom = 12; //should be challengeZoom minus 2 usually
 
 var facebookShare = 'url.com'; //link for facebook sharing
 var twitterShare = 'url.com'; //link for twitter sharing
