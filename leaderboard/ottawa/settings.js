9ec5282008febef1e2d645aae7694fcf7c6c4e3c
Edoardo Neerhut <ed@mapillary.com>
2017-08-24 22:23:35 +1000

    Update dates

--- leaderboard/ottawa/settings.js
+++ leaderboard/ottawa/settings.js
@@ -1,12 +1,12 @@
 var maxdate = Date.now(); //don't change this
-var mindate = (1471552114000); //(1502769600000); //set to show epoch milliseconds of the start date
+var mindate = (1503633600000); //(1502769600000); //set to show epoch milliseconds of the start date
 var mindateISO = new Date(mindate).toISOString();
 
 var partnerLogo = 'https://secure.meetupstatic.com/photos/event/f/7/a/highres_438483962.jpeg';
 var partnerLink = 'https://www.meetup.com/MaptimeSEA/'
 
 var challengeTitle = 'Ottawa Challenge'; //text in the slider
-var challengeDate = 'August 15 - September 15, 2017'
+var challengeDate = 'August 25 - September 17, 2017'
 var docTitle = 'Complete the Map: Ottawa - Mapillary'; //text next to favicon on browser window
 
 var challengeLng = -75.6972; //center of the map
