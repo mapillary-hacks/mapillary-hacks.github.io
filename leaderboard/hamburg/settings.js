f9365eb9e56cf7fea3fb5cb88b19a926272bc2e4
Edoardo Neerhut <Edoardo Neerhut>
2017-08-25 15:46:44 +1000

    Update share cards

--- leaderboard/hamburg/settings.js
+++ leaderboard/hamburg/settings.js
@@ -6,15 +6,15 @@ var maxdate = Date.now(); //don't change this
 var mindate = (1503655200000); //set to show epoch milliseconds of the start date
 var mindate2 = new Date(mindate).toISOString();
 
-var partnerLogo = 'https://secure.meetupstatic.com/photos/event/f/7/a/highres_438483962.jpeg';
-var partnerLink = 'https://www.meetup.com/MaptimeSEA/'
+var partnerLogo = 'https://mapillarydiscourse.s3-eu-west-1.amazonaws.com/optimized/1X/2dd358c7744b821537ddc83ac3adb4ae2a372163_1_690x414.JPG';
+var partnerLink = ''
 
 var challengeTitle = 'Hamburg Challenge'; //text in the slider
 var challengeDate = 'August 26 - September 18, 2017'
 var docTitle = 'Complete the Map: Hamburg - Mapillary'; //text next to favicon on browser window
 
-var challengeLng = 9.993653; //center of the map
-var challengeLat = 53.551043; //center of the map
+var challengeLng = 9.986326; //center of the map
+var challengeLat = 53.557719; //center of the map
 var challengeZoom = 13; //zoom for desktop browser
 var mobileZoom = 11; //should be challengeZoom minus 2 usually
 
