7071607372e7797cd5cf748d0d5a7f1af8343198
Edoardo Neerhut <Edoardo Neerhut>
2017-08-25 12:41:26 +1000

    Update dates for Hamburg challenge

--- leaderboard/hamburg/settings.js
+++ leaderboard/hamburg/settings.js
@@ -1,16 +1,16 @@
 var high = [];
 var med = [];
-var low = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25];
+var low = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16];
 
 var maxdate = Date.now(); //don't change this
-var mindate = (1503021600000); //set to show epoch milliseconds of the start date
+var mindate = (1503655200000); //set to show epoch milliseconds of the start date
 var mindate2 = new Date(mindate).toISOString();
 
 var partnerLogo = 'https://secure.meetupstatic.com/photos/event/f/7/a/highres_438483962.jpeg';
 var partnerLink = 'https://www.meetup.com/MaptimeSEA/'
 
 var challengeTitle = 'Hamburg Challenge'; //text in the slider
-var challengeDate = 'August 18th - September 15, 2017'
+var challengeDate = 'August 26 - September 18, 2017'
 var docTitle = 'Complete the Map: Hamburg - Mapillary'; //text next to favicon on browser window
 
 var challengeLng = 9.993653; //center of the map
