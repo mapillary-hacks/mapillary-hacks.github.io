63ef5cfd33fc6670331e570cf3d3e54ef8f3edb6
Edoardo Neerhut <ed@mapillary.com>
2017-09-04 18:41:33 +1000

    Fix double up zone

--- leaderboard/kyiv/settings.js
+++ leaderboard/kyiv/settings.js
@@ -1,6 +1,6 @@
 var high = [];
 var med = [1,2,3,5,6,7,9,10,11,13,14,16,18];
-var low = [4,8,9,12,15,17,19,20];
+var low = [4,8,12,15,17,19,20];
 
 var maxdate = Date.now(); //don't change this
 var mindate = (1503003600000); //set to show epoch milliseconds of the start date
