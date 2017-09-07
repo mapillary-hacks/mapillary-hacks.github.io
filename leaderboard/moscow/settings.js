7eb889dfe8b2cd8ec2fa1425026ca1fb1b4b476c
Christopher Beddow <christopher@mapillary.com>
2017-09-07 09:43:20 +0200

    Update settings.js

--- leaderboard/moscow/settings.js
+++ leaderboard/moscow/settings.js
@@ -1,6 +1,6 @@
 var high = [];
 var med = [];
-var low = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29];
+var low = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30];
 
 var maxdate = Date.now(); //don't change this
 var mindate = (1503630000000); //set to show epoch milliseconds of the start date
