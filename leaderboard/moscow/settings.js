e0743fd1c911cc5efc34be70978ca65ea0618a2c
Edoardo Neerhut <ed@mapillary.com>
2017-09-08 17:32:21 +0200

    Update centre location

--- leaderboard/moscow/settings.js
+++ leaderboard/moscow/settings.js
@@ -3,18 +3,18 @@ var med = [];
 var low = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30];
 
 var maxdate = Date.now(); //don't change this
-var mindate = (1503630000000); //set to show epoch milliseconds of the start date
+var mindate = (1505682000000); //set to show epoch milliseconds of the start date
 var mindate2 = new Date(mindate).toISOString();
 
-var partnerLogo = 'https://secure.meetupstatic.com/photos/event/f/7/a/highres_438483962.jpeg';
-var partnerLink = 'https://www.meetup.com/MaptimeSEA/'
+var partnerLogo = '';
+var partnerLink = ''
 
 var challengeTitle = 'Moscow #CompletetheMap Challenge'; //text in the slider
-var challengeDate = '11 сентября — 12 октября 2017 года'
+var challengeDate = '18 сентября — 12 октября 2017 года'
 var docTitle = 'CompletetheMap: Moscow - Mapillary'; //text next to favicon on browser window
 
-var challengeLng = 37.623281; //center of the map
-var challengeLat = 55.756486; //center of the map
+var challengeLng = 37.622509; //center of the map
+var challengeLat = 55.749355; //center of the map
 var challengeZoom = 12; //zoom for desktop browser
 var mobileZoom = 10; //should be challengeZoom minus 2 usually
 
