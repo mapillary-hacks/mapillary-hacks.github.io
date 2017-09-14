ba88939e0d1b50adc3a10f74cbad981fc598ff3f
Christopher Beddow <christopher@mapillary.com>
2017-09-14 12:57:45 +0000

    Add files via upload

---
+++ pilot/austin/mobileredirect.js
@@ -0,0 +1,7 @@
+var isMobile = function() {
+   //console.log("Navigator: " + navigator.userAgent);
+   return /(iphone|ipod|ipad|android|blackberry|windows ce|palm|symbian)/i.test(navigator.userAgent);
+ };
+ if(isMobile()) {
+        window.location.href = './m/';
+ }
---
+++ pilot/austin/settings.js
@@ -0,0 +1,25 @@
+var high = [];
+var med = [];
+var low = [];
+
+var maxdate = Date.now(); //don't change this
+var mindate = (1505347200000); //set to show epoch milliseconds of the start date
+var mindateISO = new Date(mindate).toISOString();
+var usernames = ['mly_aus01','mly_aus02','mly_aus03','mly_aus04'];
+var userkeys = ['Lpqr9GLiYJkdx0cENcAWuQ','MFS_d1p9IZqSetzDxwOjaQ','0wvc6lcZT1yjSO703yreyQ','vUdamaJh3zmWJNARQXGWYg'];
+
+var partnerLogo = '';
+var partnerLink = ''
+
+var challengeTitle = 'Austin Pilot'; //text in the slider
+var challengeDate = 'September 14th, 2017 - Current Date'
+var docTitle = 'Austin, TX - Mapillary'; //text next to favicon on browser window
+
+var challengeLng = -97.8181; //center of the map
+var challengeLat = 30.2938; //center of the map
+var challengeZoom = 12; //zoom for desktop browser
+var mobileZoom = 10; //should be challengeZoom minus 2 usually
+
+var facebookShare = 'url.com'; //link for facebook sharing
+var twitterShare = 'url.com'; //link for twitter sharing
+var emailShare = 'mail.com'; //link for email sharing
