8e62d4b56a0ced95c68803974608f7ac8ae9d0d8
Christopher Beddow <christopher@mapillary.com>
2017-09-21 20:21:48 -0600

    Add files via upload

--- pilot/austin/settings.js
+++ pilot/austin/settings.js
@@ -3,7 +3,7 @@ var med = [];
 var low = [];
 
 var maxdate = Date.now(); //don't change this
-var mindate = (1505347200000); //set to show epoch milliseconds of the start date
+var mindate = (1506063600000); //set to show epoch milliseconds of the start date
 var mindateISO = new Date(mindate).toISOString();
 var usernames = ['mly_aus01','mly_aus02','mly_aus03','mly_aus04'];
 var userkeys = ['Lpqr9GLiYJkdx0cENcAWuQ','MFS_d1p9IZqSetzDxwOjaQ','0wvc6lcZT1yjSO703yreyQ','vUdamaJh3zmWJNARQXGWYg'];
@@ -12,7 +12,7 @@ var partnerLogo = '';
 var partnerLink = ''
 
 var challengeTitle = 'Austin Pilot'; //text in the slider
-var challengeDate = 'September 14th, 2017 - Current Date'
+var challengeDate = 'September 21th, 2017 - Current Date'
 var docTitle = 'Austin, TX - Mapillary'; //text next to favicon on browser window
 
 var challengeLng = -97.8181; //center of the map
