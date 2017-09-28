81018d8a30c6d9fc6b01cf042dc9b8eaae1b1e90
Edoardo Neerhut <ed@mapillary.com>
2017-09-28 15:14:26 +1000

    Setup Maptime Milano

---
+++ leaderboard/milan/settings.js
@@ -0,0 +1,23 @@
+var high = [];
+var med = [];
+var low = [1,3,4,5,7,8,9,10,11,12,14,15,16,17,18,19,20,21,22,23,24,25];
+
+var maxdate = Date.now(); //don't change this
+var mindate = (1506808800000); //set to show epoch milliseconds of the start date
+var mindate2 = new Date(mindate).toISOString();
+
+var partnerLogo = 'https://www.facebook.com/maptimemi/';
+var partnerLink = 'http://maptime.io/milan/img/maptimemiLogo.png'
+
+var challengeTitle = 'Milano Challenge'; //text in the slider
+var challengeDate = 'Ottobre 1 - Ottobre 22, 2017'
+var docTitle = 'Complete the Map: Milano - Mapillary'; //text next to favicon on browser window
+
+var challengeLng = 9.221838645386566; //center of the map
+var challengeLat = 45.47959226728197; //center of the map
+var challengeZoom = 13; //zoom for desktop browser
+var mobileZoom = 11; //should be challengeZoom minus 2 usually
+
+var facebookShare = 'url.com'; //link for facebook sharing
+var twitterShare = 'url.com'; //link for twitter sharing
+var emailShare = 'mail.com'; //link for email sharing
