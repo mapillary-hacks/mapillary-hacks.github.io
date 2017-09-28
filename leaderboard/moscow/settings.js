cada23bf4a80c49ebd03d325a8953532ae957d7d
Edoardo Neerhut <Edoardo Neerhut>
2017-09-28 11:02:40 +1000

    Update dates

--- leaderboard/moscow/settings.js
+++ leaderboard/moscow/settings.js
@@ -3,14 +3,14 @@ var med = [];
 var low = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30];
 
 var maxdate = Date.now(); //don't change this
-var mindate = (1505682000000); //set to show epoch milliseconds of the start date
+var mindate = (1506632400000); //set to show epoch milliseconds of the start date
 var mindate2 = new Date(mindate).toISOString();
 
 var partnerLogo = '';
 var partnerLink = ''
 
 var challengeTitle = 'Moscow #CompletetheMap Challenge'; //text in the slider
-var challengeDate = '18 сентября — 12 октября 2017 года'
+var challengeDate = '29 сентября — 29 октября 2017 года'
 var docTitle = 'CompletetheMap: Moscow - Mapillary'; //text next to favicon on browser window
 
 var challengeLng = 37.622509; //center of the map
